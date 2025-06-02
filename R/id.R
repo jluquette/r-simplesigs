5cafefe3d134ef0a3247a6d65a6f6935269494f8
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2025-06-02 10:06:09 -0400

    Initial commit

---
+++ R/id.R
@@ -0,0 +1,62 @@
+#' Indel 83-dimensional signatures
+#'
+#' Create a factor with levels that represent the standard 83-dimensional
+#' indel signature channels in order. The standard format is N:\{Ins|Del\}:\{C|T|R|M\}:M
+#' where
+#' * N is the length of the indel
+#' * The indel context is: C and T represent a single base deletion of either C or T, R represents
+#'   a repeat and M represents microhomology
+#' * M is length of the context, which differs depending on context
+#'
+#' @param x Character vector of indel channels in the form specified above.
+#' @return A factor vector.
+#' @export
+id83 <- function(x=c()) {
+    id83.channel.order <- paste(
+        c(rep(1,24), rep(rep(2:5,each=6),2),c(2,3,3,4,4,4,5,5,5,5,5)),
+        c(rep(c('Del', 'Ins'), each=12), rep(c('Del', 'Ins'), each=24), rep('Del', 11)),
+        c(rep(rep(c('C','T'), each=6), 2), rep('R',48), rep('M', 11)),
+        c(rep(0:5, 12), c(1, 1,2, 1:3, 1:5)),
+        sep=':')
+    factor(x, levels=id83.channel.order)
+}
+
+id83_cols_map <- setNames(
+    rep(c('#FBBD75', '#FC7F24',   # orange
+          '#B0DB8E', '#3B9F36',   # green
+          '#FBC9B6', '#F8896D', '#EE453A', '#B91C22',    # red to pink
+          '#C5D4E4', '#8DBAD2', '#4D98C6', '#1D65A8',    # blue to light blue
+          '#E1E1EE', '#B5B6D6', '#8684BA', '#614398'),   # purples
+        c(rep(6,12), 1,2,3,5)),
+    levels(id83())
+)
+
+#' Standard colors for 83-dimensional indel signatures
+#'
+#' Map indel signatures of the form N:\{Ins|Del\}:\{C|T|R|M\}:M to the standard
+#' color, where
+#' * N is the length of the indel
+#' * The indel context is: C and T represent a single base deletion of either C or T, R represents
+#'   a repeat and M represents microhomology
+#' * M is length of the context, which differs depending on context
+#'
+#' @param x Character vector in the above form.
+#' @return A character vector of color names.
+#' @export
+id83_cols <- function(x=names(id83_cols_map)) {
+    id83_cols_map[x]
+}
+
+#' Plot ID83 spectra with ggplot
+#'
+#' A simple convenience function to quickly plot ID83 spectra. Handles coloring, ensures
+#' that ID83 channels with 0 count are not dropped, prevents a 83-element (!) color guide
+#' from being drawn, suppresses x-axis tick labels and enforces an aspect ratio.
+#'
+#' @returns A list of ggplot2 elements that can be `+`ed to a ggplot.
+#' @export
+geom_id83 <- function()
+    list(ggplot2::scale_fill_manual(values=id83_cols(), guide='none'),
+        ggplot2::geom_bar(),
+        ggplot2::scale_x_discrete(drop=FALSE),
+        ggplot2::theme(aspect.ratio=1/5, axis.text.x=ggplot2::element_blank()))
---
+++ R/sbs.R
@@ -0,0 +1,147 @@
+# Rather than do string manipulation in R, just make a hash map
+ctx_to_mutsig_map <- c(
+    `AAA:A>C`="TTT:T>G", `AAC:A>C`="GTT:T>G", `AAG:A>C`="CTT:T>G", `AAT:A>C`="ATT:T>G",
+    `CAA:A>C`="TTG:T>G", `CAC:A>C`="GTG:T>G", `CAG:A>C`="CTG:T>G", `CAT:A>C`="ATG:T>G",
+    `GAA:A>C`="TTC:T>G", `GAC:A>C`="GTC:T>G", `GAG:A>C`="CTC:T>G", `GAT:A>C`="ATC:T>G",
+    `TAA:A>C`="TTA:T>G", `TAC:A>C`="GTA:T>G", `TAG:A>C`="CTA:T>G", `TAT:A>C`="ATA:T>G",
+    `AAA:A>G`="TTT:T>C", `AAC:A>G`="GTT:T>C", `AAG:A>G`="CTT:T>C", `AAT:A>G`="ATT:T>C",
+    `CAA:A>G`="TTG:T>C", `CAC:A>G`="GTG:T>C", `CAG:A>G`="CTG:T>C", `CAT:A>G`="ATG:T>C",
+    `GAA:A>G`="TTC:T>C", `GAC:A>G`="GTC:T>C", `GAG:A>G`="CTC:T>C", `GAT:A>G`="ATC:T>C",
+    `TAA:A>G`="TTA:T>C", `TAC:A>G`="GTA:T>C", `TAG:A>G`="CTA:T>C", `TAT:A>G`="ATA:T>C",
+    `AAA:A>T`="TTT:T>A", `AAC:A>T`="GTT:T>A", `AAG:A>T`="CTT:T>A", `AAT:A>T`="ATT:T>A",
+    `CAA:A>T`="TTG:T>A", `CAC:A>T`="GTG:T>A", `CAG:A>T`="CTG:T>A", `CAT:A>T`="ATG:T>A",
+    `GAA:A>T`="TTC:T>A", `GAC:A>T`="GTC:T>A", `GAG:A>T`="CTC:T>A", `GAT:A>T`="ATC:T>A",
+    `TAA:A>T`="TTA:T>A", `TAC:A>T`="GTA:T>A", `TAG:A>T`="CTA:T>A", `TAT:A>T`="ATA:T>A",
+    `ACA:C>A`="ACA:C>A", `ACC:C>A`="ACC:C>A", `ACG:C>A`="ACG:C>A", `ACT:C>A`="ACT:C>A",
+    `CCA:C>A`="CCA:C>A", `CCC:C>A`="CCC:C>A", `CCG:C>A`="CCG:C>A", `CCT:C>A`="CCT:C>A",
+    `GCA:C>A`="GCA:C>A", `GCC:C>A`="GCC:C>A", `GCG:C>A`="GCG:C>A", `GCT:C>A`="GCT:C>A",
+    `TCA:C>A`="TCA:C>A", `TCC:C>A`="TCC:C>A", `TCG:C>A`="TCG:C>A", `TCT:C>A`="TCT:C>A",
+    `ACA:C>G`="ACA:C>G", `ACC:C>G`="ACC:C>G", `ACG:C>G`="ACG:C>G", `ACT:C>G`="ACT:C>G",
+    `CCA:C>G`="CCA:C>G", `CCC:C>G`="CCC:C>G", `CCG:C>G`="CCG:C>G", `CCT:C>G`="CCT:C>G",
+    `GCA:C>G`="GCA:C>G", `GCC:C>G`="GCC:C>G", `GCG:C>G`="GCG:C>G", `GCT:C>G`="GCT:C>G",
+    `TCA:C>G`="TCA:C>G", `TCC:C>G`="TCC:C>G", `TCG:C>G`="TCG:C>G", `TCT:C>G`="TCT:C>G",
+    `ACA:C>T`="ACA:C>T", `ACC:C>T`="ACC:C>T", `ACG:C>T`="ACG:C>T", `ACT:C>T`="ACT:C>T",
+    `CCA:C>T`="CCA:C>T", `CCC:C>T`="CCC:C>T", `CCG:C>T`="CCG:C>T", `CCT:C>T`="CCT:C>T",
+    `GCA:C>T`="GCA:C>T", `GCC:C>T`="GCC:C>T", `GCG:C>T`="GCG:C>T", `GCT:C>T`="GCT:C>T",
+    `TCA:C>T`="TCA:C>T", `TCC:C>T`="TCC:C>T", `TCG:C>T`="TCG:C>T", `TCT:C>T`="TCT:C>T",
+    `AGA:G>A`="TCT:C>T", `AGC:G>A`="GCT:C>T", `AGG:G>A`="CCT:C>T", `AGT:G>A`="ACT:C>T",
+    `CGA:G>A`="TCG:C>T", `CGC:G>A`="GCG:C>T", `CGG:G>A`="CCG:C>T", `CGT:G>A`="ACG:C>T",
+    `GGA:G>A`="TCC:C>T", `GGC:G>A`="GCC:C>T", `GGG:G>A`="CCC:C>T", `GGT:G>A`="ACC:C>T",
+    `TGA:G>A`="TCA:C>T", `TGC:G>A`="GCA:C>T", `TGG:G>A`="CCA:C>T", `TGT:G>A`="ACA:C>T",
+    `AGA:G>C`="TCT:C>G", `AGC:G>C`="GCT:C>G", `AGG:G>C`="CCT:C>G", `AGT:G>C`="ACT:C>G",
+    `CGA:G>C`="TCG:C>G", `CGC:G>C`="GCG:C>G", `CGG:G>C`="CCG:C>G", `CGT:G>C`="ACG:C>G",
+    `GGA:G>C`="TCC:C>G", `GGC:G>C`="GCC:C>G", `GGG:G>C`="CCC:C>G", `GGT:G>C`="ACC:C>G",
+    `TGA:G>C`="TCA:C>G", `TGC:G>C`="GCA:C>G", `TGG:G>C`="CCA:C>G", `TGT:G>C`="ACA:C>G",
+    `AGA:G>T`="TCT:C>A", `AGC:G>T`="GCT:C>A", `AGG:G>T`="CCT:C>A", `AGT:G>T`="ACT:C>A",
+    `CGA:G>T`="TCG:C>A", `CGC:G>T`="GCG:C>A", `CGG:G>T`="CCG:C>A", `CGT:G>T`="ACG:C>A",
+    `GGA:G>T`="TCC:C>A", `GGC:G>T`="GCC:C>A", `GGG:G>T`="CCC:C>A", `GGT:G>T`="ACC:C>A",
+    `TGA:G>T`="TCA:C>A", `TGC:G>T`="GCA:C>A", `TGG:G>T`="CCA:C>A", `TGT:G>T`="ACA:C>A",
+    `ATA:T>A`="ATA:T>A", `ATC:T>A`="ATC:T>A", `ATG:T>A`="ATG:T>A", `ATT:T>A`="ATT:T>A",
+    `CTA:T>A`="CTA:T>A", `CTC:T>A`="CTC:T>A", `CTG:T>A`="CTG:T>A", `CTT:T>A`="CTT:T>A",
+    `GTA:T>A`="GTA:T>A", `GTC:T>A`="GTC:T>A", `GTG:T>A`="GTG:T>A", `GTT:T>A`="GTT:T>A",
+    `TTA:T>A`="TTA:T>A", `TTC:T>A`="TTC:T>A", `TTG:T>A`="TTG:T>A", `TTT:T>A`="TTT:T>A",
+    `ATA:T>C`="ATA:T>C", `ATC:T>C`="ATC:T>C", `ATG:T>C`="ATG:T>C", `ATT:T>C`="ATT:T>C",
+    `CTA:T>C`="CTA:T>C", `CTC:T>C`="CTC:T>C", `CTG:T>C`="CTG:T>C", `CTT:T>C`="CTT:T>C",
+    `GTA:T>C`="GTA:T>C", `GTC:T>C`="GTC:T>C", `GTG:T>C`="GTG:T>C", `GTT:T>C`="GTT:T>C",
+    `TTA:T>C`="TTA:T>C", `TTC:T>C`="TTC:T>C", `TTG:T>C`="TTG:T>C", `TTT:T>C`="TTT:T>C",
+    `ATA:T>G`="ATA:T>G", `ATC:T>G`="ATC:T>G", `ATG:T>G`="ATG:T>G", `ATT:T>G`="ATT:T>G",
+    `CTA:T>G`="CTA:T>G", `CTC:T>G`="CTC:T>G", `CTG:T>G`="CTG:T>G", `CTT:T>G`="CTT:T>G",
+    `GTA:T>G`="GTA:T>G", `GTC:T>G`="GTC:T>G", `GTG:T>G`="GTG:T>G", `GTT:T>G`="GTT:T>G",
+    `TTA:T>G`="TTA:T>G", `TTC:T>G`="TTC:T>G", `TTG:T>G`="TTG:T>G", `TTT:T>G`="TTT:T>G"
+)
+
+
+#' Convert 192-dimensional spectra to 96
+#'
+#' There are 192 combinations of the 16 trinucleotide contexts and 12 single base
+#' changes. However, these are usually collapsed into 96 dimensions by standardizing
+#' on the pyrimidine reference base. I.e., an A>C mutation is also a T:G mutation on
+#' the opposing strand. This function maps 192-dimensional spectra to the usual
+#' 96-dimensional representation using a lookup table, specifically avoiding string
+#' parsing.  N.B. the 192-dimensional description DOES NOT refer to the commonly
+#' used transcribed strand-aware mutational signature.
+#'
+#' @param x Character vector of trinucleotide contexts plus base changes. E.g.,
+#'      "ACA:C>T".
+#' @return A factor with the ref base standardized to the pyrimidine. The factor
+#'      levels are ordered to match the standard SBS96 plots.
+#' @seealso [sbs96()]
+#' @export
+ctx_to_mutsig <- function(x) {
+    sbs96(ctx_to_mutsig_map[x])
+}
+
+#' Trinucleotide + base change 96-dimensional signatures
+#'
+#' Create a factor with levels that represent the standard 96-dimensional
+#' single base substitution (SBS) mutational signature in order.
+#'
+#' @param x Character vector with format `trinucleotide context`:`base change`
+#'      where base change is `reference base`>`alt base` and is
+#'      from the perspective of the pyrimidine at the site. For example, an A
+#'      to C mutation would be represented as T>G. A full example is: ATA:T>G.
+#' @return A factor vector.
+#' @seealso [ctx_to_mutsig] to collapse from a 192-dimensional SBS signature to
+#'      a 96-dimensional signature and factorize in one step.
+#' @export
+sbs96 <- function (x=c()) {
+    sbs96.channel.order <- paste0(rep(c("A", "C", "G", "T"),
+        each = 4), rep(c("C", "T"), each = 48), rep(c("A", "C",
+        "G", "T"), times = 4), ":", rep(c("C", "T"), each = 48),
+        ">", c(rep(c("A", "G", "T"), each = 16), rep(c("A", "C",
+            "G"), each = 16)))
+    factor(x, levels = sbs96.channel.order)
+}
+
+sbs_cols_map <- c(
+    `C>A`='deepskyblue', `C>G`='black', `C>T`='firebrick2', `T>A`='grey', `T>C`='chartreuse3', `T>G`='pink2',
+    `G>T`='deepskyblue', `G>C`='black', `G>A`='firebrick2', `A>T`='grey', `A>G`='chartreuse3', `A>C`='pink2')
+
+#' Standard colors for single base substitutions
+#'
+#' Map base change strings of the form `ref base`>`alt base` to standard colors.
+#'
+#' @param x Character vector with the form "A>C". Purine ref bases are converted
+#'      to pyrimidines. If not specified, will return a named vector of base change to
+#'      color mappings.
+#' @return A character vector of color names.
+#' @export
+sbs_cols <- function(x=names(sbs_cols_map)) {
+    sbs_cols_map[x]
+}
+
+sbs96_cols_map <- setNames(
+    sbs_cols(substr(levels(sbs96()), 5, 7)),  # AAA:C>A substr(5,7) is the muttype
+    levels(sbs96())
+)
+
+#' Standard colors for 96-dimensional single base substitution signatures
+#'
+#' Map trinucleotide context + base change strings of the form "N`ref`N:`ref base`>`alt base`
+#' to standard colors. E.g., "ACA:C>A".
+#'
+#' @param x Character vector with the form "N`ref`N:`ref`>`alt`". These character vectors
+#'      must be expressed such that ref is a pyrimidine. This requires reverse complementing
+#'      the trinucleotide context. If left unspecified, a named vector mapping each of the
+#'      possible 96 trinucleotide context+base change combinations to the standard color is
+#'      returned.
+#' @return A character vector of color names.
+#' @export
+sbs96_cols <- function(x=names(sbs96_cols_map)) {
+    sbs96_cols_map[x]
+}
+
+
+#' Plot SBS96 spectra with ggplot
+#'
+#' A simple convenience function to quickly plot SBS96 spectra. Handles coloring, ensures
+#' that SBS96 channels with 0 count are not dropped, prevents a 96-element (!) color guide
+#' from being drawn, suppresses x-axis tick labels and enforces an aspect ratio.
+#'
+#' @returns A list of ggplot2 elements that can be `+`ed to a ggplot.
+#' @export
+geom_sbs96 <- function()
+    list(ggplot2::scale_fill_manual(values=sbs96_cols(), guide='none'),
+        ggplot2::geom_bar(),
+        ggplot2::scale_x_discrete(drop=FALSE),
+        ggplot2::theme(aspect.ratio=1/5, axis.text.x=ggplot2::element_blank()))
