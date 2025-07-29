c601aaa6d244ebcd410ae185221e9b0f25b2a4c4
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2025-07-29 11:55:27 -0400

    Automatically translate COSMIC-style SBS96 channel names; add a fancy SBS96 plot function that adds a colorful strip of base changes along the top edge

--- R/sbs.R
+++ R/sbs.R
@@ -51,7 +51,66 @@ ctx_to_mutsig_map <- c(
 )
 
 
-#' Convert 192-dimensional spectra to 96
+#' Detect COSMIC-formatted SBS channels
+#'
+#' Some tools (SigProfiler*) and COSMIC represent SBS channels as N\[R>M\]N where
+#' the Ns are the upstream and downstream single bases and R and A are the reference
+#' and mutant bases, respectively. This function detects such formatting in a
+#' vectorized way, allowing conversion.
+#'
+#' @param x Character vector of channel names. May be a mixture of multiple formats.
+#' @return A boolean vector indicating which entries in `x` are in the COSMIC SBS
+#'      format: N\[R>M\]N. Note this may not be collapsed such that R is a pyrimidine.
+#' @export
+is_cosmic_sbs <- function(x) {
+    grepl(pattern='[ACGT]\\[[ACGT]>[ACGT]\\][ACGT]', x)
+}
+
+
+#' Convert COSMIC-formatted SBS channels
+#'
+#' Some tools (SigProfiler*) and COSMIC represent SBS channels as N\[R>M\]N where
+#' the Ns are the upstream and downstream single bases and R and A are the reference
+#' and mutant bases, respectively. This function converts the COSMIC format into
+#' the format used in this library:
+#'      N\[R>M\]N -> NRN:R>M
+#'
+#' @param x Character vector of SBS channel names. If `x` is already in simplesigs
+#'      format, it is returned unchanged.
+#' @return A character vector (not a factor!) in simplesigs format. Factor conversion
+#'      is not performed in case, e.g., 192-to-96 dimensional collapse is not desired.
+#' @export
+cosmic_sbs_to_simplesigs <- function(x) {
+    ifelse(is_cosmic_sbs(x),
+        paste0(substr(x,1,1), substr(x,3,3), substr(x,7,7), ':', substr(x,3,5)),
+        x)
+}
+
+
+
+#' Collapse 192 and 96-dimensional SBS spectra to 6-dimensional SBS6
+#'
+#' The SBS6 format only encodes the reference and mutated base with no local
+#' nucleotide context. I.e., NRN:R>M becomes simply R>M.
+#'
+#' @param x Character vector of any SBS channel with 1 upstream and 1 downstream
+#'      base (i.e., trinucleotide context).
+#' @param convert_cosmic If `TRUE`, convert COSMIC-style SBS channel names via
+#'      [cosmic_sbs_to_simplesigs()]. This comes with a small performance cost, so
+#'      when `x` is very large and known to be in simplesigs format, setting to
+#'      `FALSE` to skip this conversion may be useful.
+#' @return A factor vector in SBS6 order.
+#' @export
+sbs6 <- function (x=c(), convert_cosmic=TRUE) {
+    if (convert_cosmic)
+        x <- cosmic_sbs_to_simplesigs(x)
+
+    sbs6.channel.order <- c('C>A', 'C>G', 'C>T', 'T>A', 'T>C', 'T>G')
+    factor(substr(x, 5, 7), levels = sbs6.channel.order)
+}
+
+
+#' Collapse 192-dimensional spectra to 96
 #'
 #' There are 192 combinations of the 16 trinucleotide contexts and 12 single base
 #' changes. However, these are usually collapsed into 96 dimensions by standardizing
@@ -71,6 +130,7 @@ ctx_to_mutsig <- function(x) {
     sbs96(ctx_to_mutsig_map[x])
 }
 
+
 #' Trinucleotide + base change 96-dimensional signatures
 #'
 #' Create a factor with levels that represent the standard 96-dimensional
@@ -80,11 +140,18 @@ ctx_to_mutsig <- function(x) {
 #'      where base change is `reference base`>`alt base` and is
 #'      from the perspective of the pyrimidine at the site. For example, an A
 #'      to C mutation would be represented as T>G. A full example is: ATA:T>G.
+#' @param convert_cosmic If `TRUE`, convert COSMIC-style SBS channel names via
+#'      [cosmic_sbs_to_simplesigs()]. This comes with a small performance cost, so
+#'      when `x` is very large and known to be in simplesigs format, setting to
+#'      `FALSE` to skip this conversion may be useful.
 #' @return A factor vector.
 #' @seealso [ctx_to_mutsig] to collapse from a 192-dimensional SBS signature to
 #'      a 96-dimensional signature and factorize in one step.
 #' @export
-sbs96 <- function (x=c()) {
+sbs96 <- function (x=c(), convert_cosmic=TRUE) {
+    if (convert_cosmic)
+        x <- cosmic_sbs_to_simplesigs(x)
+
     sbs96.channel.order <- paste0(rep(c("A", "C", "G", "T"),
         each = 4), rep(c("C", "T"), each = 48), rep(c("A", "C",
         "G", "T"), times = 4), ":", rep(c("C", "T"), each = 48),
@@ -154,4 +221,115 @@ geom_sbs96 <- function(tx=FALSE) {
         ggplot2::scale_x_discrete(drop=FALSE),
         ggplot2::scale_y_continuous(expand=ggplot2::expansion(c(0, 0.05))),
         ggplot2::theme(aspect.ratio=1/4, axis.text.x=ggplot2::element_blank()))
+}
+
+
+#' Fancy SBS96 spectrum plot
+#'
+#' Make a fancier SBS96 spectrum plot with a header row of colored boxes describing the
+#' base changes and monospaced font x-axis labels describing the trinucleotide contexts.
+#' This plot hijacks [facet_grid()] to create the top-strip of colored boxes, so is not
+#' as flexible as [geom_sbs96()] and is generally only useful to plot one or a very
+#' small number of mutation signatures. This is a great choice for plotting a single
+#' signature in a publication figure. Requires the ggh4x library.
+#' IMPORTANT: this function hijacks facet_grid to create the colorful strip of mutation
+#' types along the top edge. So the usual flexibility and utility of faceting is lost
+#' when using this style of plot. 
+#'
+#' @param data data.frame containing a character column of SBS96 channels and a
+#'      numeric weight column. IMPORTANT! Unlike other plotting functions, `data`
+#'      must contain explicit entries for all SBS96 channels (e.g., 0s)!
+#' @param mutsig Unquoted expression yielding a character vector suitable for conversion
+#'      by [sbs96()]. Usually the name of the column in `data` containing SBS96 channels. Do
+#'      not specify this as a string.
+#' @param weight Unquoted expression to compute channel weights (bar heights). Usually
+#'      the name of the column in `data`.
+#' @param rows Unquoted expression to define rows in plot since facet_grid is hijacked.
+#' @param aspect.ratio Numeric value describing the aspect ratio of all 6 panels combined.
+#' @return A ggplot object.
+#' @export
+plot_fancy_sbs96 <- function(data, mutsig, weight, rows=NULL, aspect.ratio=1/4) {
+    color_strip <- ggh4x::strip_themed(
+        background_x=ggh4x::elem_list_rect(linewidth=rep(0, 6), fill=unique(sbs96_cols())),
+        text_x=ggh4x::elem_list_text(face=rep('bold',6), colour=c(rep(c('white'), each=6)))
+    )
+    ggplot2::ggplot(data, ggplot2::aes(x=sbs96({{ mutsig }}), fill=sbs96({{ mutsig }}), weight={{ weight }})) +
+        ggplot2::scale_fill_manual(values=sbs96_cols(), guide='none') +
+        ggplot2::geom_bar() + #width=1) +
+        ggplot2::geom_vline(xintercept=16 + 0.9/2) +    # geom_bar width is 0.9 by default
+        ggplot2::theme_classic() +
+        ggplot2::scale_y_continuous(expand=ggplot2::expansion(c(0, 0.05))) +
+        ggplot2::xlab('Trinucleotide context') +
+        ggplot2::ylab(ggplot2::element_blank()) +
+        ggplot2::scale_x_discrete(
+            drop=TRUE,
+            expand=ggplot2::expansion(c(0,0)),
+            breaks=levels(sbs96()),
+            labels=substr(levels(sbs96()), 1, 3)) + 
+        ggplot2::theme(aspect.ratio=6 * aspect.ratio,
+            panel.spacing=ggplot2::unit(0.1, units='mm'),
+            axis.text.x=ggplot2::element_text(angle=90, size=5, hjust=1, vjust=1/2, family='mono')) +
+        ggh4x::facet_grid2(rows=ggplot2::vars({{ rows }}),
+            cols=ggplot2::vars(sbs6( {{ mutsig }})), scale='free_x', strip=color_strip)
+}
+
+
+#' Order trinucleotide contexts
+#'
+#' @param x Character vector for which the first 3 letters in each element are
+#'      trinucleotides.
+#' @param convert_cosmic If `TRUE`, convert COSMIC-style SBS channel names via
+#'      [cosmic_sbs_to_simplesigs()]. This comes with a small performance cost, so
+#'      when `x` is very large and known to be in simplesigs format, setting to
+#'      `FALSE` to skip this conversion may be useful.
+#' @return A factor vector of flanking nucleotides in the SBS96 order.
+#' @export
+ctx3 <- function(x=c(), convert_cosmic=TRUE) {
+    if (convert_cosmic)
+        x <- cosmic_sbs_to_simplesigs(x)
+
+    ctx3.channel.order <- c(
+        'A_A', 'A_C', 'A_G', 'A_T',
+        'C_A', 'C_C', 'C_G', 'C_T',
+        'G_A', 'G_C', 'G_G', 'G_T',
+        'T_A', 'T_C', 'T_G', 'T_T')
+    factor(paste0(substr(x,1,1), '_', substr(x,3,3)), levels=ctx3.channel.order)
+}
+
+
+#' Unsuccessful attempt to make plot_fancy_sbs96 work for non-full spectra
+#'
+#' plot_fancy_sbs96 requires values for all 96 SBS96 channels which can be inconvenient.
+#' Tried to make a function that would automatically handle the case where not all values
+#' are present *without manipulating the data*. Looks like this isn't possible, but this
+#' is the closest I've gotten. The main problem is using facet_grid to layout the colored
+#' strip along the top of the plot. Using faceting in this way causes so many unusual
+#' problems.
+#' @param data don't use
+#' @param mutsig don't use
+#' @param weight don't use
+#' @param aspect.ratio don't use
+#' @return don't use
+#'
+plot_fancy_sbs96_2 <- function(data, mutsig, weight=1, aspect.ratio=1/4) {
+    color_strip <- ggh4x::strip_themed(
+        background_x=ggh4x::elem_list_rect(linewidth=rep(0, 6), fill=unique(sbs96_cols())),
+        text_x=ggh4x::elem_list_text(face=rep('bold',6), colour=c(rep(c('white'), each=6)))
+    )
+    ggplot2::ggplot(data, ggplot2::aes(x=ctx3({{ mutsig }}), fill=sbs96({{ mutsig }}), weight={{ weight }})) +
+        ggplot2::scale_fill_manual(values=sbs96_cols(), guide='none') +
+        ggplot2::geom_bar() + #width=1) +
+        ggplot2::geom_vline(xintercept=16 + 0.9/2) +    # geom_bar width is 0.9 by default
+        ggplot2::theme_classic() +
+        ggplot2::scale_y_continuous(expand=ggplot2::expansion(c(0, 0.05))) +
+        ggplot2::xlab('Trinucleotide context') +
+        ggplot2::ylab(ggplot2::element_blank()) +
+        ggplot2::scale_x_discrete(
+            drop=FALSE,
+            expand=ggplot2::expansion(c(0,0))) +
+            #labels=substr(levels(sbs96()), 1, 3)) + 
+        ggplot2::theme(aspect.ratio=6 * aspect.ratio,
+                       panel.spacing=ggplot2::unit(0.1, units='mm'),
+                       axis.text.x=ggplot2::element_text(angle=90, size=5, hjust=1, vjust=1/2, family='mono')) +
+        ggh4x::facet_grid2(cols=ggplot2::vars(sbs6( {{ mutsig }})), scale='free_x', strip=color_strip, drop=FALSE)
 }
